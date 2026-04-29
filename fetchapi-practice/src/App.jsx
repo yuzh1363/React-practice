545a2740c7ae89aa6ac320e591552f43f41ded48
yuzh1363 <yuzh0806@gmail.com>
2026-04-29 13:15:49 +0800

    feat: 增加即時搜尋功能

--- fetchapi-practice/src/App.jsx
+++ fetchapi-practice/src/App.jsx
@@ -4,9 +4,11 @@ import { getUser } from "./api";
 function App() {
     const [post, setPost] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [input, setInput] = useState("");
 
     const handlefetchData = async () => {
         setIsLoading(true);
+        setInput("");
         try {
             const result = await getUser();
             setPost(result);
@@ -16,6 +18,10 @@ function App() {
             setIsLoading(false);
         }
     };
+
+    const searchPost = post.filter((item) => {
+        return item.title.toLowerCase().includes(input.toLowerCase());
+    });
     return (
         <>
             <div className="flex flex-col my-5 items-center gap-5">
@@ -28,8 +34,17 @@ function App() {
                     onClick={handlefetchData}
                     disabled={isLoading}
                 >
-                    載入資料
+                    {post.length === 0 ? "載入資料" : "重新載入"}
                 </button>
+                <div>
+                    <input
+                        className="border rounded-md pl-2"
+                        placeholder="請輸入關鍵字"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <h2 className="text-center">搜尋筆數：{input === "" ? "0" :searchPost.length}</h2>
+                </div>
             </div>
 
             <hr />
@@ -37,19 +52,29 @@ function App() {
                 "載入中"
             ) : (
                 <div className="m-10 flex flex-wrap justify-around gap-5">
-                    {post.map((item) => {
-                        return (
-                            <div
-                                key={item.id}
-                                className="max-w-72 border-2 rounded-lg p-5"
-                            >
-                                <h2 className="font-bold underline text-indigo-500">
-                                    {item.title}
-                                </h2>
-                                <p>內文：{item.body}</p>
-                            </div>
-                        );
-                    })}
+                    {searchPost.length > 0 ? (
+                        searchPost.map((item) => {
+                            return (
+                                <div
+                                    key={item.id}
+                                    className="max-w-72 border-2 rounded-lg p-5"
+                                >
+                                    <h2 className="font-bold underline text-indigo-500">
+                                        {item.title}
+                                    </h2>
+                                    <p>內文：{item.body}</p>
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <div className="text-center w-full mt-10">
+                            <p className="text-gray-400 text-xl">
+                                {post.length === 0
+                                    ? "請先點擊「載入資料」"
+                                    : "找不到符合的卡片"}
+                            </p>
+                        </div>
+                    )}
                 </div>
             )}
         </>
